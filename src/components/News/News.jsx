16760f4f8a26d79567acc368bb2a23511e485e08
Nein243 <ysvalera@gmail.com>
2022-10-06 22:00:49 +0300

    2 commit

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Post from '../Post/Post';
 import NewsStyle from './News.module.css';
 
-const PostItem = (props) => {
-    return(
-        <div className={NewsStyle.postItem}>
-            
-        </div>
-    )
-}
-
 const News = function () {
+
+    let posts = [
+        { id: 1, name: 'Denis Denisov', text: 'That is a cool platform', likes: 2, comments: 0 },
+        { id: 1, name: 'Petro Petrov', text: 'Why do nobody text me?', likes: 48, comments: 9 },
+        { id: 1, name: 'Ivan Ivanov', text: 'Glat to meet you here!', likes: 8, comments: 1 },
+        { id: 1, name: 'Valerii Holubiev', text: 'Hey there! It\'s my first post!', likes: 12, comments: 3 },
+    ]
+
+    let postItems = posts.map(post => <Post id={post.id} name={post.name} text={post.text} likes={post.likes} comments={post.comments} />)
+
+
     return (
         <section className={NewsStyle.news}>
             <h1>My Posts</h1>
@@ -17,6 +21,7 @@ const News = function () {
                 <textarea></textarea>
                 <button className={NewsStyle.postButton}>Add Post</button>
             </div>
+            {postItems}
 
 
         </section>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import PostStyle from './Post.module.css';
 
-const Post = function () {
+const Post = function (props) {
     return (
-        
+        <div className={PostStyle.post}>
+            <div className={PostStyle.postAvatar}>
+                <img src="https://secure.gravatar.com/avatar/6f2b5cd69cdc5a925be2f496efd5ffba?s=96&d=mm&r=g" alt="Avatar" />
+            </div>
+            <div className={PostStyle.postContent}>
+                <div className={PostStyle.postName}>{props.name}</div>
+                <div className={PostStyle.postText}>{props.text}</div>
+            </div>
+            <div className={PostStyle.postFeedback}>
+                <div className={PostStyle.postLikes}>{props.likes} likes</div>
+                <div className={PostStyle.postComments}>{props.comments} comments</div>
+            </div>
+        </div>
 
     )
 }
